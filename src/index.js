d123f9e2a7c909a67cf57400e5dd22e85c37aa80
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 22:15:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function reverse (n) {
-  let nStr = String (n);
+  let N = Math.abs(n);
+  let nStr = String (N);
   let nRev = nStr.split("").reverse().join("");
   return (Number (nRev));
 }
